1b9d9ec8de2b719b22c7b353ac61f7a6e01cd653
zgl <guoliuzhang@foxmail.com>
2023-04-30 11:26:14 +0800

    开发核心功能：发送私信

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -258,4 +258,8 @@ public class UserService implements CommunityConstant {
 
         return map;
     }
+
+    public User findUserByName(String username) {
+        return userMapper.selectByName(username);
+    }
 }
